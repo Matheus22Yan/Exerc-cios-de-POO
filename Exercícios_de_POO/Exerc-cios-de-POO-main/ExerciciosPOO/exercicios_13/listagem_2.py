3141a206cc9b63a60ec882337d4195a37ae29d5b
Matheus Yan <msantos0005@gmail.com>
2025-05-24 14:03:00 -0300

    Add files via upload

--- Exercícios_de_POO/Exerc-cios-de-POO-main/ExerciciosPOO/exercicios_13/listagem_2.py
+++ Exercícios_de_POO/Exerc-cios-de-POO-main/ExerciciosPOO/exercicios_13/listagem_2.py
@@ -1,20 +1,37 @@
-import tkinter as tk 
+import tkinter as tk
 from tkinter import ttk
 
-class Aplicacao(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.contador_1 = 0
-        self.contador_2 = 0
-        self.title("Contadores")
-        self.geometry("250x100")
-        self.cria_quadro()
+contador_1 = 0
+contador_2 = 0
 
-    def cria_quadro(self):
-        self.quadro = ttk.Frame(self)
-        self.l_contador_1 = ttk.Label(
-            self.quadro, text=self.formata_contador(1, self.contador_1))
-        self.l_contador_1.pack()
-        self.botao_1 = ttk.Button(
-            self.quadro, text="Adiciona ao contador 1", command=self.conta_1)  
-        
+
+def formata_contador(contador, valor):
+    return f"Contador {contador}: {valor}"
+
+
+def conta_1():
+    global contador_1, l_contador_1
+    contador_1 += 1
+    l_contador_1["text"] = formata_contador(1, contador_1)
+
+
+def conta_2():
+    global contador_2, l_contador_2
+    contador_2 += 1
+    l_contador_2["text"] = formata_contador(2, contador_2)
+
+
+raiz = tk.Tk()
+raiz.title("Contadores")
+raiz.geometry("250x100")
+quadro = ttk.Frame(raiz)
+l_contador_1 = ttk.Label(quadro, text=formata_contador(1, contador_1))
+l_contador_1.pack()
+botao_1 = ttk.Button(quadro, text="Adiciona ao contador 1", command=conta_1)
+botao_1.pack()
+l_contador_2 = ttk.Label(quadro, text=formata_contador(2, contador_2))
+l_contador_2.pack()
+botao_2 = ttk.Button(quadro, text="Adiciona ao contador 2", command=conta_2)
+botao_2.pack()
+quadro.pack(expand=True)
+raiz.mainloop()
--- Exercícios_de_POO/Exerc-cios-de-POO-main/ExerciciosPOO/exercicios_13/listagem_3.py
+++ Exercícios_de_POO/Exerc-cios-de-POO-main/ExerciciosPOO/exercicios_13/listagem_3.py
@@ -1,33 +1,47 @@
 import tkinter as tk
 from tkinter import ttk
 
-contador_1 = 0
-contador_2 = 0
 
-def formata_contador (contador, valor): 
-    return f"Contador (contador): (valor)"
+class Aplicacao(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.contador_1 = 0
+        self.contador_2 = 0
+        self.title("Contadores")
+        self.geometry("250x100")
+        self.cria_quadro()
 
-def conta_1(): 
-    global contador_1, l_contador_1
-    contador_1 += 1
-    l_contador_1["text"] = formata_contador(1, contador_1)
+    def cria_quadro(self):
+        self.quadro = ttk.Frame(self)
+        self.l_contador_1 = ttk.Label(
+            self.quadro, text=self.formata_contador(1, self.contador_1)
+        )
+        self.l_contador_1.pack()
+        self.botao_1 = ttk.Button(
+            self.quadro, text="Adiciona ao contador 1", command=self.conta_1
+        )
+        self.botao_1.pack()
+        self.l_contador_2 = ttk.Label(
+            self.quadro, text=self.formata_contador(2, self.contador_2)
+        )
+        self.l_contador_2.pack()
+        self.botao_2 = ttk.Button(
+            self.quadro, text="Adiciona ao contador 2", command=self.conta_2
+        )
+        self.botao_2.pack()
+        self.quadro.pack(expand=True)
 
-def conta_2():
-    global contador_2, l_contador_2
-    contador_2 += 1
-    l_contador_2["text"] = formata_contador(2, contador_2)
+    def formata_contador(self, contador, valor):
+        return f"Contador {contador}: {valor}"
 
-raiz = tk.Tk()
-raiz.title("Contadores")
-raiz.geometry("250x100")
-quadro = ttk.Frame(raiz)
-l_contador_1 = ttk.Label(quadro, text=formata_contador(1, contador_1))
-l_contador_1.pack()
-botao_1 = ttk.Button(quadro, text="Adiciona ao contador 1", command=conta_1)
-botao_1.pack()
-l_contador_2 = ttk.Label(quadro, text=formata_contador(2, contador_2))
-l_contador_2.pack()
-botao_2 = ttk.Button(quadro, text="Adiciona ao contador 2", command=conta_2)
-botao_2.pack()
-quadro.pack(expand=True)
+    def conta_1(self):
+        self.contador_1 += 1
+        self.l_contador_1["text"] = self.formata_contador(1, self.contador_1)
+
+    def conta_2(self):
+        self.contador_2 += 1
+        self.l_contador_2["text"] = self.formata_contador(2, self.contador_2)
+
+
+raiz = Aplicacao()
 raiz.mainloop()
