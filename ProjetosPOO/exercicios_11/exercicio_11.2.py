92e4afe9935e1e0ab100cfed544494df2f3e909b
Matheus Yan <msantos0005@gmail.com>
2025-05-27 22:25:44 -0300

    Update exercicio_11.2.py

--- ProjetosPOO/exercicios_11/exercicio_11.2.py
+++ ProjetosPOO/exercicios_11/exercicio_11.2.py
@@ -3,14 +3,6 @@ from contextlib import closing
 
 with sqlite3.connect("precos.db") as conexao:
     with closing(conexao.cursor()) as cursor:
-
-        # cursor.execute(
-        #     """
-        #     create table precos(
-        #     nome text,
-        #     preco numeric)
-        # """
-        # )
         cursor.execute("delete from precos")
         cursor.executemany(
             """
