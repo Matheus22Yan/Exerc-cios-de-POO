9f58154857dcc5bf4080b16c4657f5c444bf6bda
Matheus Yan <msantos0005@gmail.com>
2025-05-27 22:46:05 -0300

    Update exercicio_11.5.py

--- ProjetosPOO/exercicios_11/exercicio_11.5.py
+++ ProjetosPOO/exercicios_11/exercicio_11.5.py
@@ -3,12 +3,6 @@ from contextlib import closing
 
 with sqlite3.connect("precos.db") as conexao:
     with closing(conexao.cursor()) as cursor:
-        # cursor.execute(
-        #     """update precos
-        #                set preco = preco * 1.21"""
-        # )
-
-        # conexao.rollback()
         cursor.execute("""select * from precos""")
         for resultado in cursor.fetchall():
             print("Nome: {0:30s} Preço {1:6.2f}".format(*resultado))
